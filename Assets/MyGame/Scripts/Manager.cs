d67773adc5123c19e9640d5e1d19b36c6c404cd8
Jakob Emanuel Berger <130294565+jakob-berger-c@users.noreply.github.com>
2024-12-05 08:46:04 +0100

    readme lessons learned

--- Assets/MyGame/Scripts/Manager.cs
+++ Assets/MyGame/Scripts/Manager.cs
@@ -4,33 +4,42 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
-    [SerializeField] private TableLayoutData tablelayout;
-
-    [SerializeField] private StudentData[] studens;
-
-    [SerializeField] private GameObject tablePrefab;
-
-    [SerializeField] private GameObject chairPrefab;
-
-    
+    [SerializeField] private TableLayoutData tableLayout; //Ref zu TableLayout ScriptableObject
+    [SerializeField] private StudentData[] students; //
+    [SerializeField] private GameObject tablePrefab; //Prefab für Tisch
+    [SerializeField] private GameObject chairPrefab; //Prefab für Sessel
 
     // Start is called before the first frame update
     private void Start()
     {
-        for(int row = 0; row < tablelayout.rows; row++)
+        for (int row = 0; row < tableLayout.rows; row++)
         {
-            for (int col = 0; col < tablelayout.columns; col++)
+            for (int col = 0; col < tableLayout.columns; col++)
             {
-                GameObject table = Instantiate(tablePrefab);
-                Debug.Log("Table created");
-               
+                Vector3 tablePosition = new Vector3(col * tableLayout.tableSpacing, 0, row * tableLayout.tableSpacing);
+
+                //Tische platzieren
+                GameObject table = Instantiate(tablePrefab, tablePosition, Quaternion.identity, transform);
+                table.name = "Table " + row + " " + col;
+                table.transform.position = tablePosition;
+                //Sessel platzieren (2 pro Tisch)
+                // Sessel platzieren
+                Transform pos1 = table.transform.Find("pos1");
+                Transform pos2 = table.transform.Find("pos2");
+
+                if (pos1)
+                {
+                    Instantiate(chairPrefab, pos1.position, pos1.rotation, table.transform);
+                }
+
+                if (pos2)
+                {
+                    Instantiate(chairPrefab, pos2.position, pos2.rotation, table.transform);
+                    //Transform[]
+                }
+
             }
         }
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- README.md
+++ README.md
@@ -5,5 +5,10 @@ Erstellung von einem Sitzplan In Unity mithilfe von:
 - statisch, dynamischer Koordinatenzuweisung
 - Prefabs
 - AudioClpis
-https://teams.microsoft.com/l/message/48:notes/1733297681124?context=%7B%22contextType%22%3A%22chat%22% 
+https://teams.microsoft.com/l/message/48:notes/1733297681124?context=%7B%22contextType%22%3A%22chat%22%7D
 
+https://github.com/user-attachments/assets/3cfcdd92-0fa7-46c1-8a7c-647bc17ea54e
+
+# Lessons Learned:
+- Unity macht manchmal Sachen die man nicht erwarte
+- Prefabs sind eine wichtige Komponente
